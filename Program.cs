17a1890c1bfe6ae19c9476bdfd6f1a445374512a
Sofia <SofiaCandiottiS@gmail.com>
2025-11-04 16:41:36 -0500

    Subiendo datos personales

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T3_PRÁCTICA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi nombre es Monica Sofia Candiotti Simeon. ");
+        }
+    }
+}
